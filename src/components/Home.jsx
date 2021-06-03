4ea2041a7fbe27fa7bd909c00496f0691c910656
anisur2805 <anisur2805@gmail.com>
2021-06-03 12:59:50 +0600

    format component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,25 @@
-import React, { useState } from 'react'
-import { Button, Modal } from 'reactstrap';
-import MultiStepForm from './Multi-stepForm';
-
+import React, { useState } from "react";
+import { Button, Modal } from "reactstrap";
+import MultiStepForm from "./Multi-stepForm";
 
 const Home = () => {
+  const [modalState, setModalState] = useState(false);
+  const toggle = () => setModalState(!modalState);
 
-	const [ modalState, setModalState ] = useState(false)
-	const toggle = () => setModalState(!modalState)
+  return (
+    <div>
+      <div className="modal-wrapper ">
+        <h1>Multi Step Modal</h1>
+        <Button color="danger" className="btn" onClick={toggle}>
+          Open Form Modal
+        </Button>
+      </div>
 
-	return (
-		<div>
-			<div className="modal-wrapper ">
-				<h1>Multi Step Modal</h1>
-				<Button color="danger" className="btn" onClick={ toggle }>Open Form Modal</Button>
-			</div>
+      <Modal isOpen={modalState} toggle={toggle}>
+        <MultiStepForm toggle={toggle} />
+      </Modal>
+    </div>
+  );
+};
 
-			<Modal isOpen={ modalState } toggle={ toggle }>
-				<MultiStepForm toggle={ toggle } />
-			</Modal>
-
-
-		</div>
-	)
-}
-
-export default Home
+export default Home;
